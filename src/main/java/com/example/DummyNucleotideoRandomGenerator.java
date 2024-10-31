3a2dc4ef199131eb21fdc542eab75bd53572cbae
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-10-30 21:13:43 -0300

    fix: add dummy generator

---
+++ src/main/java/com/example/DummyNucleotideoRandomGenerator.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import java.util.Random;
+
+public class DummyNucleotideoRandomGenerator implements NucleotideoRandomGenerator {
+    private final char[] NUCL = {'A', 'C', 'T', 'G'};
+    private final Random random = new Random();;
+
+    public String generate(int sequenceSize) {
+        char randomNucleotideo = this.NUCL[random.nextInt(this.NUCL.length)];
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < sequenceSize; i++) {
+            stringBuilder.append(randomNucleotideo);
+        }
+
+        return stringBuilder.toString();
+    }
+
+} 
